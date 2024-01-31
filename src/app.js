15b295fb327a9ef8eb499279c330186b7a8bc15d
IyiolaJay <jimoh.iyiola11@gmail.com>
2024-01-31 10:45:49 +0100

    hot fix

--- src/app.js
+++ src/app.js
@@ -17,6 +17,10 @@ app.use(
       saveUninitialized: true,
       maxAge: 24 * 60 * 60 * 1000,
       proxy: true,
+      cookie:{
+        sameSite : 'none',
+        secure : true,
+      }
     })
   );
   
